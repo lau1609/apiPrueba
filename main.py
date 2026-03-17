e907535ed0d4d9f43553966719383c125942afd3
lau1609 <109751592+lau1609@users.noreply.github.com>
2026-03-17 13:53:36 -0600

    Create main.py
    Inicio de prueba. Primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+# Creamos la instancia de la aplicación
+app = FastAPI()
+
+@app.get("/")
+def mensaje_de_prueba():
+    return {"status": "viva", "mensaje": "Hola Laura, tu API en el VPS funciona!"}
+
+@app.get("/calculo/{n1}/{n2}")
+def sumar(n1: int, n2: int):
+    resultado = n1 + n2
+    return {"operacion": "suma", "resultado": resultado}
